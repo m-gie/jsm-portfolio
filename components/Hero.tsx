a8a16d282f6961b263fca2094b2e1fd8d4787a7e
m.glowacki <m.glowacki@smsnet.pl>
2024-05-27 15:20:02 +0200

    Add About section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
-import { TextGenerateEffect } from "./ui/text-generate-effect";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import MagicButton from "./ui/MagicButton";
 import Link from "next/link";
 import { FaLocationArrow } from "react-icons/fa6";
@@ -20,7 +20,6 @@ const Hero = () => {
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
       <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
-        {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
       <div className="flex justify-center relative my-20 z-10">
@@ -33,7 +32,7 @@ const Hero = () => {
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi, I&apos;m a web developer with a passion for creating dynamic web
+            Hi, I&apos;m a web developer with passion for creating dynamic web
             apps
           </p>
           <Link href="#about">
