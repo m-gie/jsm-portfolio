85a8d0a184e074bf1a78af7347d8834ac7cf315a
m.glowacki <m.glowacki@smsnet.pl>
2024-05-27 11:27:56 +0200

    Add Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
+import MagicButton from "./ui/MagicButton";
+import Link from "next/link";
+import { FaLocationArrow } from "react-icons/fa6";
 
 const Hero = () => {
   return (
@@ -10,10 +14,36 @@ const Hero = () => {
           fill="white"
         />
         <Spotlight
-          className="top-10 left-full h-[80vh] w-[50vh]"
+          className="top-10 left-full h-[80vh] w-[50vw]"
           fill="purple"
         />
-        <Spotlight className="top-28 left-80 h-[80vh] w-[50vh]" fill="blue" />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      </div>
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic web content with Next.js
+          </h2>
+          <TextGenerateEffect
+            words="Transforming Concepts into Seamless User Experiences"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi, I&apos;m a web developer with a passion for creating dynamic web
+            apps
+          </p>
+          <Link href="#about">
+            <MagicButton
+              title="See my work"
+              position="right"
+              icon={<FaLocationArrow />}
+            />
+          </Link>
+        </div>
       </div>
     </div>
   );
